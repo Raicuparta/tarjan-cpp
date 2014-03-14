181404d0e55dfe2df0922688a81c98e3cc754d95
Raicuparta <raicuparta@gmail.com>
2014-03-14 19:34:58 +0000

    a bia nao trabalha

--- projecto1.cpp
+++ projecto1.cpp
@@ -1,1 +1,46 @@
- 
+include <stdio.h>
+include <iostream>
+
+class Pessoa {
+private:
+	Pessoa* _partilhas;
+	int _numeroPartilhas;
+	int _id;
+
+public:
+	Pessoa(id) {
+		_id = id;
+		_numeroPartilhas = 0;
+	}
+
+	Pessoa* getPartilhas() {
+		return _partilhas;
+	}
+
+	void addPartilha(Pessoa p){
+		_partilhas[_numeroPartilhas] = p;
+		_numeroPartilhas++;
+	}
+
+	int getID() {
+		return _id;
+	}
+
+};
+
+
+
+int main(){
+		
+	int n, p;
+	
+	scanf("%d %d", n, p);
+	
+	Pessoa partilhas[n];
+	
+	for (int i = 0; i < n; i++) {
+		
+	}
+	
+	
+}
