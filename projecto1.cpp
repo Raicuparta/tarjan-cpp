2b770b4ad66ee5b60cf06de98da89232f092a1db
Raicuparta <raicuparta@mail.com>
2014-03-14 22:59:39 +0000

    inputs

--- projecto1.cpp
+++ projecto1.cpp
@@ -1,29 +1,38 @@
-include <stdio.h>
-include <iostream>
+#include <stdio.h>
+#include <iostream>
 
 class Pessoa {
 private:
-	Pessoa* _partilhas;
+	Pessoa** _partilhas;
 	int _numeroPartilhas;
 	int _id;
 
 public:
-	Pessoa(id) {
-		_id = id;
+
+	Pessoa() {
+		_partilhas = new Pessoa*;
+		_id = 0;
 		_numeroPartilhas = 0;
 	}
 
-	Pessoa* getPartilhas() {
+	Pessoa** getPartilhas() {
 		return _partilhas;
 	}
+	
+	int getNumeroPartilhas() {
+		return _numeroPartilhas;
+	}
 
-	void addPartilha(Pessoa p){
-		_partilhas[_numeroPartilhas] = p;
-		_numeroPartilhas++;
+	void addPartilha(Pessoa* p){
+		_partilhas[_numeroPartilhas++] = p;
 	}
 
 	int getID() {
-		return _id;
+		return _id + 1;
+	}
+	
+	void setID(int id) {
+		_id = id;
 	}
 
 };
@@ -34,13 +43,28 @@ int main(){
 		
 	int n, p;
 	
-	scanf("%d %d", n, p);
+	scanf("%d %d", &n, &p);
 	
-	Pessoa partilhas[n];
-	
+	Pessoa* pessoas = new Pessoa[n];
 	for (int i = 0; i < n; i++) {
-		
+		pessoas[i].setID(i);
 	}
 	
+	int pess1, pess2;
+	for (int i = 0; i < p; i++) {
+		scanf("%d %d", &pess1, &pess2);
+		pess1--;
+		pess2--;
+		pessoas[pess1].addPartilha(&pessoas[pess2]);		
+	}
+	
+	
+	//TESTES FIXES
+	for (int i = 0; i < n; i++) {
+		printf("Pessoa %d:\n", i+1);
+		for (int j = 0; j < pessoas[i].getNumeroPartilhas(); j++) {
+			printf("-%d\n", pessoas[i].getPartilhas()[j]->getID());
+		}
+	}
 	
 }
